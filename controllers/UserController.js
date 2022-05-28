e518226c37df5dec3b4ff6edd4cb8a642d1cc2c4
HedgehogKU <ccc908925@gmail.com>
2022-05-28 17:43:33 +0800

    ADD: API: post /users/sign_up

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,8 +1,11 @@
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const UserModel = require('../models/User');
-const appError = require('../service/appError');
-const success = require('../service/responseSuccess');
+
+const appError = require('../utils/appError');
+const success = require('../services/responseSuccess');
+const generateJWT = require('../utils/generateJWT');
 
 module.exports = {
     async getUser(req, res, next) {
@@ -17,10 +20,15 @@ module.exports = {
             sex,
             email,
             password,
+            confirmPassword,
         } = data;
 
-        if ( !name ) {
+        if ( !name.trim() ) {
             return appError('【暱稱】必填', next);
+        }
+
+        if ( !validator.isLength(name, { min: 2 }) ) {
+            return appError('【暱稱】至少 2 個字元以上', next);
         }
 
         if ( !sex ) {
@@ -31,15 +39,43 @@ module.exports = {
             return appError('【帳號】必填', next);
         }
 
-        if ( !password ) {
+        if ( !validator.isEmail(email) ) {
+            return appError('請輸入正確信箱格式', next);
+        }
+
+        if ( !password.trim() ) {
             return appError('【密碼】必填', next);
+        }
+
+        if ( !confirmPassword.trim() ) {
+            return appError('【確認密碼】必填', next);
+        }
+
+        if ( !validator.isLength(password, { min: 8 }) ) {
+            return appError('【密碼】需 8 碼以上', next);
+        }
+
+        if ( !validator.isStrongPassword(password) ) {
+            return appError('【密碼】需包含各一個大小寫英文、數字和符號', next);
+        }
+
+        if ( validator.matches(password, /['<>]/g) ) {
+            return appError("【密碼】請勿使用 ' < >", next);
+        }
+
+        if ( password !== confirmPassword ) {
+            return appError('【密碼】不一致', next);
+        }
+
+        const isExistedEmail = await UserModel.find({ email });
+        if ( isExistedEmail.length > 0 ) {
+            return appError('【帳號】已被註冊', next);
         }
 
         data.password = await bcrypt.hash(data.password, 12);
 
-        const result = await UserModel.create(data);
-        result.password = undefined;
+        const user = await UserModel.create(data);
 
-        success(res, result, 201);
+        generateJWT(user, res, 201);
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const handleErrorAsync = require('../service/handleErrorAsync');
+const handleErrorAsync = require('../utils/handleErrorAsync');
 const UserController = require('../controllers/UserController');
 
+router.post('/sign_up', handleErrorAsync(UserController.insertUser));
 router.get('/:id', handleErrorAsync(UserController.getUser));
 
 module.exports = router;
